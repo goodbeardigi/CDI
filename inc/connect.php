00c64a23f867f0fad9de7ba89c316204494f1932
Cobbalobb <james.d.cobbett@gmail.com>
2014-02-12 16:33:55 +0000

    Fix weird not scrolling bug linked to connection check

--- inc/connect.php
+++ inc/connect.php
@@ -1,13 +1,4 @@
 <?php
 // Create connection
 $con=mysqli_connect("10.168.1.52","cdisport_cdi","zQDjg3ybyWQKcqs","cdisport_db");
-
-// Check connection
-if (mysqli_connect_errno($con))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
- else {
- 	echo "Connected to database.";
- }
 ?>
