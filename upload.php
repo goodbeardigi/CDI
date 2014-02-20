2072fb6158b735938f56f1dfe66d9e00ff68ba28
Cobbalobb <james.d.cobbett@gmail.com>
2014-02-20 12:34:04 +0000

    add connection

--- upload.php
+++ upload.php
@@ -1,21 +1,9 @@
 <?php
 
-//include('inc/connect.php');
+include('inc/connect.php');
 include('header.php');
-
-$id=$_GET["id"];
-
-$result = mysqli_query($con,"SELECT * FROM videos WHERE id=$id");
-
-while($row = mysqli_fetch_array($result)){
-	$title = $row['name'];
-	$description = $row['description'];
-	$user_id = $row['user_id'];
-	$categories_id = $row['categories_id'];
-	$date_added = $row['date_added'];
-	$url = $row['url'];
-}
 ?>
+
 <!-- Main hero unit for a primary marketing message or call to action -->
 <div class="hero-unit">
 		<div class="container">
