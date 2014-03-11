246e44f687791a1c4b57a5465b262acc865d8702
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-11 22:53:58 +0000

    sql

--- video.php
+++ video.php
@@ -6,9 +6,10 @@ include('inc/timeago.php');
 
 $id=$_GET["id"];
 
-$result = mysqli_query($con,"SELECT videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, users.username, users.id FROM videos INNER JOIN users ON videos.user_id=users.id'");
+$result = mysqli_query($con,"SELECT videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, users.username, users.id FROM videos INNER JOIN users ON videos.user_id=users.id WHERE videos.id='$id' ");
 
 while($row = mysqli_fetch_array($result)){
+print_r($result);
 	$title = $row['name'];
 	$description = $row['description'];
 	$user_id = $row['user_id'];
