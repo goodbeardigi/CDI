7961b7aa3eb7897c812fc4752821493cfedf5ce3
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-12 17:42:42 +0000

    Add views

--- inc/addview.php
+++ inc/addview.php
@@ -4,9 +4,8 @@ include('connect.php');
 
 // Get values from form 
 $id=$_POST['id'];
-
 // Insert data into mysql 
-$sql="UPDATE video SET views = views+1 WHERE id='$id'";
+$sql="UPDATE videos SET views = views+1 WHERE id='$id'";
 $result=mysqli_query($con, $sql);
 
 // close connection 
