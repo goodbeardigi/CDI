f395764d61b3d0088ebb94441313945d44d625f4
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-12 17:56:28 +0000

    fix rows cat

--- category.php
+++ category.php
@@ -5,7 +5,7 @@ include('header.php');
 include('inc/timeago.php');
 
 $id=$_GET["id"];
-
+$i=0;
 $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.categories_id=$id");
 ?>
 <div class="container">
@@ -13,6 +13,9 @@ $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, 
 				<div class="row-fluid">
 					<?php
 					while($row = mysqli_fetch_array($result)){
+						if($i=0){
+							?><div class="row-fluid"><?php
+						}
 						?>
 				        <div class="span4 video text-center box"> 
 				        	<a href="/video.php?id=<?php echo $row['id'];?>" class="video-overlay">
@@ -23,9 +26,11 @@ $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, 
 				        	</a>    
 				       		<img src="images/video-temp.jpg">    
 				        </div>
-					<?php } ?>
-			        
-			     </div>
+				        <?php if($i=3){
+							?></div><?php
+							$i=-1;
+						} ?>
+					<?php $i++; } ?>
 	</div>
 </div>
 <?php include('footer.php'); ?>
