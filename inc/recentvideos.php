24b076468c8eb9af709e086ff0087d65f0bc25b5
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-17 22:57:17 +0000

    Fix ids

--- inc/recentvideos.php
+++ inc/recentvideos.php
@@ -9,6 +9,7 @@ $recentvideos = array();
 $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id ORDER BY videos.id DESC LIMIT 6");
 
 while($row = mysqli_fetch_array($result)){
+	$recentvideos[$i]['id'] = $row['id'];
 	$recentvideos[$i]['title'] = $row['name'];
 	$recentvideos[$i]['description'] = $row['description'];
 	$recentvideos[$i]['user_id'] = $row['user_id'];
