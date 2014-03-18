58f5e9cbd99a500175ca6c02485a51729147418a
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-18 17:08:22 +0000

    fix spacing home

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ include('header.php');
 <section id="Section-1">
 	<div class="container">
     <div class="row">
-		<div class="span12">
+		<div class="span12 reducebottom">
 			<ul  id="myTab"  class="video-grid-nav nav nav-tabs"> 
 				<li><a href="#recent-videos" class="active" data-toggle="tab">Recent videos</a></li>
 				<li><a href="#weeks-hottest" data-toggle="tab">Weeks Hottest</a></li>
