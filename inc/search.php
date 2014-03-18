ce52b0c75e5c0d0fd2a9c8468cfc7b636f06c659
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-18 08:36:26 +0000

    search

---
+++ inc/search.php
@@ -0,0 +1,22 @@
+<?php
+
+include('connect.php');
+
+$term=$_GET["term"];
+$i=0;
+$videos = array();
+
+$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.name LIKE '%$term%'");
+
+while($row = mysqli_fetch_array($result)){
+	$videos[$i]['id'] = $row['id'];
+	$videos[$i]['title'] = $row['name'];
+	$videos[$i]['description'] = $row['description'];
+	$videos[$i]['user_id'] = $row['user_id'];
+	$videos[$i]['category'] = $row['categoriy'];
+	$videos[$i]['date_added'] = $row['date_added'];
+	$videos[$i]['url'] = $row['url'];
+	$i++;
+}
+echo json_encode($recentvideos);
+?>
