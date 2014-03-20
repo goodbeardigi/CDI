c1ae34c4770916b732374b4d2cfef0f1ffc9a668
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-20 23:43:06 +0000

    contact us

---
+++ thank-you.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Thank you!</title>
+      <link rel="STYLESHEET" type="text/css" href="contact.css">
+</head>
+<body>
+
+<h2>Thanks for contacting us!</h2>
+
+</body>
+</html>
