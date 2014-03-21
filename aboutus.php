fb49ad8307e759c4053dff416dd0b4cc0eddb020
Al3x29 <cam00766@myport.ac.uk>
2014-03-21 15:41:24 +0000

    Minor about us tweaks

--- aboutus.php
+++ aboutus.php
@@ -28,14 +28,14 @@ include('header.php');
 					<table style=" text-align:center;padding-left:10px; padding-right:10px; padding-bottom:10px">
 							<tr>
 								<td style="width:100%; color:#0099FF; font-size: 100px;">
-								<h1><b>ABOUT CDI SPORTS</b></h1>
+								<h1><b>ABOUT Xtv SPORTS</b></h1>
 								</td>
 							</tr>
 						
 							<tr>
 								<td style="width:100%; padding:10px;">
 								<h3>
-								The Group consists of Alex Ackland, James Cobbett, Tom Marples, Luke O'Brien, Hulya Roxana and Madalina Timofte.<br> We are all Computing and Digital Image students giving the name CDI Sports.
+								The Group consists of Alex Ackland, James Cobbett, Tom Marples, Luke O'Brien, Hulya Roxana and Madalina Timofte.<br> We are all Computing and Digital Image students in our final year.
 								</h3>
 								</td>
 							</tr>
@@ -43,19 +43,24 @@ include('header.php');
 						
 					<table style="text-align:justify;">
 							<tr>
-								<td style="width:50%; padding-left:15px; border-top: 3px solid blue; ">
-									<h2 style="color:#0099FF;">Lorem Ipsum</h2>
+								<td style="width:45%; padding-left:15px; border-top: 3px solid #0097FC ">
+									<h2 style="color:#0099FF;">Expirience</h2>
 								</td>
-								<td style="width:50%; padding-left:15px; border-top: 3px solid blue; ">
-									<h2 style="color:#0099FF;">Lorem Ipsum</h2>
+								<td style="width:5%; padding-left:15px; border-top: 3px solid #0097FC ">
+									<h2 style="color:#0099FF;"></h2>
+								</td>
+								<td style="width:45%; padding-left:15px; border-top: 3px solid #0097FC; ">
+									<h2 style="color:#0099FF;">Social</h2>
 								</td>
 							</tr>
 							<tr>
-								<td style="width:50%;padding15px;">
-								The site will host a variety of videos from different sports, all hosted internally. CDI Sports aims to combine great videos with a clean, modern looking interface, which provides a very user friendly experience.
-						
-								</td>				
-								<td style ="width:50%; padding:15px;">
+								<td style="width:45%;padding-top: 0px;padding-bottom: 40px;">
+								The site will host a variety of videos from different sports, which will be hosted internally. Xtv Sports aims to combine great videos with a clean, modern looking interface, which provides a very user friendly experience.
+								</td>
+								<td style="width 10%;">
+								</td>
+												
+								<td style="width:45%;padding-top: 0px;padding-bottom: 40px;padding-right: 20px;">
 								Videos will have the ability to be rated, commented on, and shared on social media sites via the built in links. These features will be accessible to people who have signed up and created an account.
 								</td>
 							</tr>
