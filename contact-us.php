76e878c60ec68f4cf1b7f8a8f80198f0301a76e5
Al3x29 <cam00766@myport.ac.uk>
2014-03-23 14:04:34 +0000

    contact page standardisation

--- contact-us.php
+++ contact-us.php
@@ -13,26 +13,26 @@ include('header.php');
 					<p>Fill in the form below, and we'll get back to you within 24 hours.</p>
 					<div>
 						<label>
-							<span><h3>Name: (required)</h3></span>
-							<input style = "border-radius: 0; width 90%; " placeholder="Please enter your name" type="text" tabindex="1" required autofocus>
+							<span><h3>Name:</h3></span>
+							<input style = "border-radius: 4px; width 90%; " placeholder="Please enter your name" type="text" tabindex="1" required autofocus>
 						</label>
 					</div>
 					<div>
 						<label>
-							<span><h3>Email: (required)</h3></span>
-							<input style = "border-radius: 0;" placeholder="Please enter your email address" type="email" tabindex="2" required>
+							<span><h3>Email:</h3></span>
+							<input style = "border-radius: 4px;" placeholder="Please enter your email address" type="email" tabindex="2" required>
 						</label>
 					</div>
 					<div>
 						<label>
-							<span><h3>Telephone: (required)</h3></span>
-							<input style = "border-radius: 0;" placeholder="Please enter your number" type="tel" tabindex="3" required>
+							<span><h3>Telephone:</h3></span>
+							<input style = "border-radius: 4px;" placeholder="Please enter your number" type="tel" tabindex="3" required>
 						</label>
 					</div>
 					<div>
 						<label>
-							<span><h3>Message: (required)</h3></span>
-							<textarea style = "border-radius: 0;" placeholder="Include all the details you can" tabindex="5" required></textarea>
+							<span><h3>Message:</h3></span>
+							<textarea style = "border-radius: 4px;" placeholder="Include all the details you can" tabindex="5" required></textarea>
 						</label>
 					</div>
 					<div>
