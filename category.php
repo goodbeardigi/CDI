cc3e77329634379ca72debb3e100a5e90c8497db
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-25 13:52:46 +0000

    fix cat page

--- category.php
+++ category.php
@@ -9,7 +9,7 @@ $i=0;
 $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, videos.length, videos.image, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.categories_id=$id");
 ?>
 <div class="container">
-	<h2 id="searchterm"><?php $row=mysqli_fetch_array($result); echo $row[0]['category'];?></h2>
+	<h2 id="searchterm"><?php $row=mysqli_fetch_array($result); echo $row['category'];?></h2>
 	<div id="cat-videos" class=" tab-pane fade active in video-grid">
 					<?php
 					while($row = mysqli_fetch_array($result)){
@@ -18,13 +18,13 @@ $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, 
 						}
 						?>
 				        <div class="span4 video text-center box"> 
-				        	<a href="/video.php?id=<?php echo $row['id'];?>" class="video-overlay">
+				        	<a href="/video.php?id=<?php echo $row[$i]['id'];?>" class="video-overlay">
 				        		<div>
-				        			<h2><?php echo $row['name'];?></h2>
-				        			<span><?php echo $row['category'];?></span>
+				        			<h2><?php echo $row[$i]['name'];?></h2>
+				        			<span><?php echo $row[$i]['category'];?></span>
 				        		</div>
 				        	</a>    
-				       		<img src="videos/thumbnails/<?php echo $row['image'] ?>" alt="<?php echo $row['name'] ?>" />
+				       		<img src="videos/thumbnails/<?php echo $row[$i]['image'] ?>" alt="<?php echo $row[$i]['name'] ?>" />
 				        </div>
 				        <?php if($i===2){
 							?></div><?php
