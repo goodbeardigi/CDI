4129d55f7aa6f0a50fe0980ff76a4227dd29d22e
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-25 13:38:25 +0000

    update featured image

--- category.php
+++ category.php
@@ -6,7 +6,7 @@ include('inc/timeago.php');
 
 $id=$_GET["id"];
 $i=0;
-$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.categories_id=$id");
+$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, videos.length, videos.image, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.categories_id=$id");
 ?>
 <div class="container">
 	<h2 id="searchterm"><?php $row=mysqli_fetch_array($result); echo $row[0]['category'];?></h2>
@@ -24,7 +24,7 @@ $result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, 
 				        			<span><?php echo $row['category'];?></span>
 				        		</div>
 				        	</a>    
-				       		<img src="images/video-temp.jpg">    
+				       		<img src="videos/thumbnails/<?php echo $row['image'] ?>" alt="<?php echo $row['name'] ?>" />
 				        </div>
 				        <?php if($i===2){
 							?></div><?php
