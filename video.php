2783e7fdeb73adeefb9a754e6a9323f009040faf
Cobbalobb <james.d.cobbett@gmail.com>
2014-04-08 13:48:01 +0100

    add div for videos

--- video.php
+++ video.php
@@ -115,6 +115,7 @@ while($row = mysqli_fetch_array($result)){
 			<div class="span5">
 				<h3>THIS WEEKS HOTTEST //</h3>
 				<script type="text/javascript">$( document ).ready(function() {hottestvideos_videopage(days);});</script>	
+				<div id="weeks-hottest">  </div>
 			<div>	
 		</div>
 	</div>
