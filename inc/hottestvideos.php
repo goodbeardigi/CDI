fa54086a0e7ad8dee03178feb81fee48158a97c0
Cobbalobb <james.d.cobbett@gmail.com>
2014-04-15 00:18:50 +0100

    fix home cat

--- inc/hottestvideos.php
+++ inc/hottestvideos.php
@@ -9,7 +9,7 @@ $p=0;
 //$hottestvideos[5]['title']=test;
 //$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id LIMIT 6");
 
-$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, videos.length, videos.image, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE vidoes.date_added BETWEEN current_date()-$days AND current_date() ORDER BY videos.views DESC LIMIT 6");
+$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, videos.length, videos.image, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE date_added BETWEEN current_date()-$days AND current_date() ORDER BY views DESC LIMIT 6");
 
 while($row = mysqli_fetch_array($result)){
 	$videos[$p]['title'] = $row['name'];
