13e241b23bda5d20dcdf8c2b2a49f38bed9a710c
Al3x29 <cam00766@myport.ac.uk>
2014-04-14 23:30:59 +0100

    text and structure changes to about us page

--- about-us.php
+++ about-us.php
@@ -7,7 +7,7 @@ include('header.php');
 	  <!-- Carousel items -->
 		<div class="carousel-inner">
 			<div class="active item">
-				<div class="row">
+				<div class="row" style="padding-bottom: 0px;">
 					<div class="span12 text-left">
 						<img src="images/surf-girl.jpg" alt="" />
 					</div>
@@ -25,43 +25,43 @@ include('header.php');
 			<div class="span12">
 				<div class="content">
 						
-					<table style=" text-align:center;padding-left:10px; padding-right:10px; padding-bottom:10px">
+					
 							<tr>
 								<td style="width:100%; color:#3B5FBD; font-size: 100px;">
-								<h1><b>ABOUT Xtv SPORTS</b></h1>
+								<h2 style="text-align: center; display: block;"><b>ABOUT Xtv SPORTS</b></h2>
 								</td>
 							</tr>
-						
 							<tr>
-								<td style="width:100%; padding:10px;">
-								<h3>
-								The Group consists of Alex Ackland, James Cobbett, Tom Marples, Luke O'Brien, Hulya Roxana and Madalina Timofte.<br> We are all Computing and Digital Image students in our final year.
-								</h3>
+								<td style="width:100%; text-align:center;">
+								<h3 style="text-align: center; display: block;">
+								Founded in 2013, Xtv allows people to discover, watch and share extreme sports videos.
 								</td>
 							</tr>
-					</table>
+					
 						
-					<table style="text-align:justify;">
+					<table style="text-align:justify; margin-top:6px;">
 							<tr>
 								<td style="width:45%; padding-left:15px; border-top: 3px solid #3B5FBD ">
-									<h2 style="color:#3B5FBD;">Expirience</h2>
+									<h3 style="color:#3B5FBD; text-align: center;
+display: block;">Experience</h3>
 								</td>
 								<td style="width:5%; padding-left:15px; border-top: 3px solid #3B5FBD ">
 									<h2 style="color:#3B5FBD;"></h2>
 								</td>
 								<td style="width:45%; padding-left:15px; border-top: 3px solid #3B5FBD; ">
-									<h2 style="color:#3B5FBD;">Social</h2>
+									<h3 style="color:#3B5FBD; text-align: center;
+display: block;">Social</h3>
 								</td>
 							</tr>
 							<tr>
 								<td style="width:45%;padding-top: 0px;padding-bottom: 40px;">
-								The site will host a variety of videos from different sports, which will be hosted internally. Xtv Sports aims to combine great videos with a clean, modern looking interface, which provides a very user friendly experience.
+								The site will host a variety of videos from different  extreme sports. Xtv Sports aims to combine great videos with a clean, modern looking interface, providing a very user friendly experience.
 								</td>
 								<td style="width 10%;">
 								</td>
 												
 								<td style="width:45%;padding-top: 0px;padding-bottom: 40px;padding-right: 20px;">
-								Videos will have the ability to be rated, commented on, and shared on social media sites via the built in links. These features will be accessible to people who have signed up and created an account.
+								Videos will have the ability to be rated, commented on, and shared on social media sites via the built in links. These features will allow you to connect and interact with those who love the same sports.
 								</td>
 							</tr>
 					</table>	
