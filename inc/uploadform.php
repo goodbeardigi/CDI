4f8edb9afb2e6fe19d7c79bb70275f7d56e480c3
Cobbalobb <james.d.cobbett@gmail.com>
2014-04-14 20:32:04 +0100

    fix image upload

--- inc/uploadform.php
+++ inc/uploadform.php
@@ -8,7 +8,7 @@ $categories_id =  $_POST['categories'];
 $date_added =  date("Y-m-d");
 $url =  $_POST['url'];
 $url = str_replace(" ", "-", $url);
-$image = $url."jpg";
+$image = $url.".jpg";
 //$length = "";
 
 $ffmpeg = "ffmpeg/ffmpeg";
