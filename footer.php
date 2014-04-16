de028a7030a9e1afcf514274f3cc40e99c46a65a
Al3x29 <cam00766@myport.ac.uk>
2014-04-16 16:34:43 +0100

    making text bigger in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
 <!-- FOOTER -->
 <footer id="foot-sec">
-	<div class="one">
+	<div class="one" style="font-size: 13px;">
   <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports</p>
   <a style="margin-left:25px; display: -webkit-inline-box;" href="category.php?id=0">SKATE</a>
   <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=1">SURF</a>
