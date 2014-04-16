8b8a78fc366d151eec2fd24319a4bc40db8ff1e6
Al3x29 <cam00766@myport.ac.uk>
2014-04-16 16:31:09 +0100

    neating up footer code

--- footer.php
+++ footer.php
@@ -2,18 +2,18 @@
 <!-- FOOTER -->
 <footer id="foot-sec">
 	<div class="one">
-  <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports
-  <a style="margin-left:25px;" href="category.php?id=0">SKATE</a>
-  <a style="margin-left:8px;" href="category.php?id=1">SURF</a>
-  <a style="margin-left:8px;" href="category.php?id=2">SKY</a>
-  <a style="margin-left:8px;" href="category.php?id=3">SNOW</a>
-  <p style="margin-left:8px; display: -webkit-inline-box; color: #FFF">|
-  <a style="margin-left:8px;" href="about-us.php">ABOUT</a>
-  <a style="margin-left:8px;" href="contact-us.php">CONTACT</a>
-  <a style="margin-left:8px;" href="#" onclick="$('#loginModal').modal('show')">SIGN IN</a>
-  <a style="margin-left:8px;" href="upload.php">UPLOAD</a>
-
-  </p>
+  <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports</p>
+  <a style="margin-left:25px; display: -webkit-inline-box;" href="category.php?id=0">SKATE</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=1">SURF</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=2">SKY</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=3">SNOW</a>
+  <p style="margin-left:8px; display: -webkit-inline-box; color: #FFF">|</p>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="about-us.php">ABOUT</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="contact-us.php">CONTACT</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="#" onclick="$('#loginModal').modal('show')">SIGN IN</a>
+  <a style="margin-left:8px; display: -webkit-inline-box;" href="upload.php">UPLOAD</a>
+</div>
+  
 </div>
 </footer>
 
