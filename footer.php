c5cfd0617fbdf853ba4442ee5015c962dbbcff6f
Al3x29 <cam00766@myport.ac.uk>
2014-04-16 14:35:07 +0100

    Adding links to footer

--- footer.php
+++ footer.php
@@ -1,8 +1,22 @@
 
 <!-- FOOTER -->
 <footer id="foot-sec">
-  <p style="margin-left: 20px">&copy; 2014 Xtv Sports</p>
+	<div class="one">
+  <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports
+  <a style="margin-left:25px;" href="category.php?id=0">SKATE</a>
+  <a style="margin-left:8px;" href="category.php?id=1">SURF</a>
+  <a style="margin-left:8px;" href="category.php?id=0">SKY</a>
+  <a style="margin-left:8px;" href="category.php?id=1">SNOW</a>
+  <p style="margin-left:8px; display: -webkit-inline-box; color: #FFF">|
+  <a style="margin-left:8px;" href="about-us.php">ABOUT</a>
+  <a style="margin-left:8px;" href="contact-us.php">CONTACT</a>
+  <a style="margin-left:8px;" href="#" onclick="$('#loginModal').modal('show')">SIGN IN</a>
+  <a style="margin-left:8px;" href="upload.php">UPLOAD</a>
+
+  </p>
+</div>
 </footer>
+
 <!-- / FOOTER -->
 
 </body>
