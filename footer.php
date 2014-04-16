10fed7b7a5d33e6713009b7280d84b71462b87b5
Al3x29 <cam00766@myport.ac.uk>
2014-04-16 14:44:29 +0100

    2 link changes

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
   <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports
   <a style="margin-left:25px;" href="category.php?id=0">SKATE</a>
   <a style="margin-left:8px;" href="category.php?id=1">SURF</a>
-  <a style="margin-left:8px;" href="category.php?id=0">SKY</a>
-  <a style="margin-left:8px;" href="category.php?id=1">SNOW</a>
+  <a style="margin-left:8px;" href="category.php?id=2">SKY</a>
+  <a style="margin-left:8px;" href="category.php?id=3">SNOW</a>
   <p style="margin-left:8px; display: -webkit-inline-box; color: #FFF">|
   <a style="margin-left:8px;" href="about-us.php">ABOUT</a>
   <a style="margin-left:8px;" href="contact-us.php">CONTACT</a>
