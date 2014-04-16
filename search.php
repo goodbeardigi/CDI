f5f6ff28dcc6f804c27fcf42bc8e78114f34d3af
Cobbalobb <james.d.cobbett@gmail.com>
2014-04-16 15:23:23 +0100

    improve search

--- search.php
+++ search.php
@@ -9,7 +9,7 @@ $i=0;
 $videos = array();
 
 if($term != ""){
-	$result = mysqli_query($con,"SELECT videos.id, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.name LIKE '%$term%' OR videos.description LIKE '%$term%'");
+	$result = mysqli_query($con,"SELECT videos.id, videos.image, videos.name, videos.description, videos.user_id, videos.categories_id, videos.date_added, videos.url, categories.category FROM videos INNER JOIN categories ON categories.id=videos.categories_id WHERE videos.name LIKE '%$term%' OR videos.description LIKE '%$term%'");
 
 ?>
 <div class="container">
@@ -21,14 +21,14 @@ if($term != ""){
 							?><div class="row-fluid"><?php
 						}
 						?>
-				        <div class="span4 video text-center box"> 
+				        <div class="col-xs-12 col-sm-5 col-md-4 col-lg-4 video text-center box"> 
 				        	<a href="/video.php?id=<?php echo $row['id'];?>" class="video-overlay">
 				        		<div>
 				        			<h2><?php echo $row['name'];?></h2>
 				        			<span><?php echo $row['category'];?></span>
 				        		</div>
 				        	</a>    
-				       		<img src="images/video-temp.jpg">    
+				       		<img src="videos/thumbnails/<?php echo $row['image'] ?>">    
 				        </div>
 				        <?php if($i===2){
 							?></div><?php
