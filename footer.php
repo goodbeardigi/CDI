fae87dda1ad5f4a7092f6dfea53876eff6da2007
Al3x29 <cam00766@myport.ac.uk>
2014-04-17 11:08:12 +0100

    putting it into 2 divs - footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <footer id="foot-sec">
 	<div class="one" style="font-size: 13px;">
   <p style="margin-left:20px; color:#919191; display: -webkit-inline-box;">&copy; 2014 Xtv Sports</p>
+  		<div class="two" style="display: -webkit-inline-box;">
   <a style="margin-left:25px; display: -webkit-inline-box;" href="category.php?id=0">SKATE</a>
   <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=1">SURF</a>
   <a style="margin-left:8px; display: -webkit-inline-box;" href="category.php?id=2">SKY</a>
@@ -12,6 +13,7 @@
   <a style="margin-left:8px; display: -webkit-inline-box;" href="contact-us.php">CONTACT</a>
   <a style="margin-left:8px; display: -webkit-inline-box;" href="#" onclick="$('#loginModal').modal('show')">SIGN IN</a>
   <a style="margin-left:8px; display: -webkit-inline-box;" href="upload.php">UPLOAD</a>
+</div>
 </div>
   
 </footer>
