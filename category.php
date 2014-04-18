79d50284d26aaf1f05856e46cec0d2ad9f98b726
Cobbalobb <james.d.cobbett@gmail.com>
2014-04-18 12:21:17 +0100

    change infinity settings

--- category.php
+++ category.php
@@ -57,8 +57,8 @@ $(document).ready(function() {
     $('#cat-videos').scrollPagination({
  		
  		cat_id 	: "<?php echo $id ?>",
-        nop     : 10, // The number of posts per scroll to be loaded
-        offset  : 2, // Initial offset, begins at 0 in this case
+        nop     : 3, // The number of posts per scroll to be loaded
+        offset  : 12, // Initial offset, begins at 0 in this case
         error   : 'No More Posts!', // When the user reaches the end this is the message that is
                                     // displayed. You can change this if you want.
         delay   : 500, // When you scroll down the posts will load after a delayed amount of time.
